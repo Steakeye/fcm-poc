770d25bb89dbf89bc44ca94d97b7c2ae203e07ce
Andrew Keats <andrew.keats@git.live>
2021-07-21 19:06:19 +0100

    adding core app model and initialising it

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import firebase from "firebase/app";
 import '@firebase/messaging';
 import firebaseConfig from './firebase.config.json';
+import { AppModel } from '@gitliveapp/core';
 
 console.log('logging main.js');
+
+const appModel = AppModel({
+    version: `1.0.0`,
+    store(key, value) {
+        console.log(`store`, key, value);
+        localStorage.setItem(key, value);
+    },
+    retrieve(key, type) {
+        console.log(`retrieve`, key, type);
+        return localStorage.getItem(key) ?? undefined;
+    },
+    retrieveOrDefault(key, defaultValue) {
+        const resourceValue  = localStorage.getItem(key);
+
+        return resourceValue ?? defaultValue;
+    },
+    clear(key) {
+        localStorage.clear(key);
+    },
+    reportException(exception) {
+        console.error(`AppModel Error`, exception);
+    },
+});
+
+appModel.urlOpened(location.href);
 
 if (`serviceWorker` in navigator) {
     // Use the window load event to keep the page load performant
