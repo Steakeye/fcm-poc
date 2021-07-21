8dee1b82795d42ba9f210587d775653cbe3a0a5a
Andrew Keats <andrew.keats@git.live>
2021-07-21 02:18:12 +0100

    adding firebase dep

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,4 +1,12 @@
+import firebase from "firebase";
+import 'firebase/messaging';
 import firebaseConfig from './firebase.config.json';
 
 console.log('logging serviceWorker.js');
 console.log('firebaseConfig', firebaseConfig);
+
+const { vapidKey, ...firebaseInitOptions } = firebaseConfig;
+
+firebase.initializeApp(firebaseInitOptions);
+
+const messaging = firebase.messaging();
